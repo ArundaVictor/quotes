69b59d028f00316706f45891e179976c8ba54563
Arunda Victor <victoroyugi31@gmail.com>
2019-01-13 23:17:59 +0300

    add styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
+    AppRoutingModule,
     FormsModule
   ],
   providers: [],
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,4 @@
 export class Quotes {
-  constructor(public id:number, public name:string,public description:string){}
+  constructor(){}
 }
+   // public id:number, public name:string,public description:string
