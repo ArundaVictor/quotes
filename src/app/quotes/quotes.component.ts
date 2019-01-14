d60d3c5a3c9912ed487da38eb3d3098104672439
Arunda Victor <victoroyugi31@gmail.com>
2019-01-14 12:55:29 +0300

    add toggle button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -6,12 +6,16 @@ import {Quotes} from '../quotes'
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  
+
   quotes =[
-    new Quotes (1, "Everyday is a brand new day", "Everyday is beautiful"),
-    new Quotes (2, "Everyday is a brand new day", "Everyday is beautiful"),
-    new Quotes (3, "Everyday is a brand new day", "Everyday is beautiful"),
+    new Quotes (1, "Everyday is a brand new day", "Victor"),
+    new Quotes (2, "Which day", "Chege"),
+    new Quotes (3, "Today", "Dami"),
   ]
+  toogleDetails(index){
+        this.quotes[index].showDescription = !this.quotes[index].showDescription;
+    }
+
   constructor() { }
 
   ngOnInit() {
